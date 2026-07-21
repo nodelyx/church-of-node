6be8c5e6a35db257d56386a6f603571666714a91
nodelyx <nodelyx@gmail.com>
2026-07-21 18:05:02 +1000

    Add Reply model

---
+++ models/Reply.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ReplySchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  thread: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Thread',
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Reply', ReplySchema);

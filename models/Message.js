6c5baad5e635115ad55b809dc0fdf46d13018a7b
nodelyx <nodelyx@gmail.com>
2026-07-22 09:41:04 +1000

    Create Message model for group chat

---
+++ models/Message.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
